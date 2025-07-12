97e70998d1b4aaf4396e3a3f407baab4dd6d4006
Lidia Asamnew <lidia.yirbe@gmail.com>
2025-07-12 20:58:04 +0300

    Create Diagona-traverse.py

---
+++ Diagona-traverse.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        ans = []
+        rows, cols = len(mat), len(mat[0])
+        row, col = 0, 0
+        going_up = True
+
+        while len(ans) < rows*cols:
+            if going_up:
+                while row >= 0 and col<cols:
+                    ans.append(mat[row][col])
+                    row -= 1
+                    col += 1
+                if col== cols:
+                    row += 2
+                    col -= 1
+                else:
+                    row += 1
+                going_up = False
+            else:
+                while col>=0 and row<rows:
+                    ans.append(mat[row][col])
+                    col -= 1
+                    row += 1
+                if row == rows:
+                    col += 2
+                    row -= 1
+                else:
+                    col += 1
+                going_up = True
+        return ans
